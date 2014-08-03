304cd7ee9a8000b91918e0ead647d332a9d2e354
Jean-Louis Marechaux <jlmarech@ca.ibm.com>
2014-08-03 21:33:37 +0000

    removed spaces

--- routes/sales.js
+++ routes/sales.js
@@ -2,22 +2,21 @@
 
 
 exports.getSales = function(resA, customer) {
-	return [
-        {
-        	"objectId" : "2B402DCC-3AF6-4771-9418-MD1011JLMB7H3",
-			"createdAt" : 1393276905592,
-			"modifiedAt" : 1393276905592,
-			"className" : "SpecialItem",
-			"version" : "0",
-			"attributes" : {
-				"description" : "JLM Full Suspension EX",
-				"img" : "MountainBike_1.png",
-				"name" : "JLM Full Suspension EX",
-				"category" : "MountainBike",
+	return [ {
+		"objectId" : "2B402DCC-3AF6-4771-9418-MD1011JLMB7H3",
+		"createdAt" : 1393276905592,
+		"modifiedAt" : 1393276905592,
+		"className" : "SpecialItem",
+		"version" : "0",
+		"attributes" : {
+			"description" : "JLM Full Suspension EX",
+			"img" : "MountainBike_1.png",
+			"name" : "JLM Full Suspension EX",
+			"category" : "MountainBike",
 			"inStock" : "Reduced 25%",
-				"sizes" : [],
-				"price" : "$1495.99"
-          	 }
-        }
-     ];
-};
+			"sizes" : [],
+			"price" : "$1495.99"
+						}
+			}
+		];
+	};
