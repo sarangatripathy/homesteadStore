46f2778802d827fc0ca82c681c043e85ee593cf0
Jean-Louis Marechaux <jlmarech@ca.ibm.com>
2014-08-04 21:36:57 +0000

    bikes

--- routes/index.js
+++ routes/index.js
@@ -64,7 +64,7 @@ exports.products = function(req, res) {
 // Render the Bikes page
 exports.bikes = function(req, res) {
 	res.render('bikes', {
-		bicycles : specials.getSpecialsBikes();
+		bicycles : specials.getSpecialsBikes()
 	});
 };
 
