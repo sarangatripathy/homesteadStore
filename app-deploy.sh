fd5d52767f1bce150b0bee56c8a214064ad1a7c4
Jean-Louis Marechaux <jlmarech@ca.ibm.com>
2014-12-17 21:24:53 -0600

    New deploy script

--- app-deploy.sh
+++ app-deploy.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Get the Bluemix Org
+# Get the Bluemix Org and the App name
 ORG=${CF_ORG}
+APP=${CF_APP}
 
 # Get the Bluemix App name
-echo ${CF_APP}
+# echo ${CF_APP}
 
-# Replace non-alpha , non-numeric characters in Org name
+# Replace non-alpha , non-numeric characters in Org name and App name
 ORG_ID=${ORG//[^a-zA-Z0-9_-]/-}
+APP_ID=${APP//[^a-zA-Z0-9_-]/-}
 
 # Create a custom host based on App name and Org name
-Host_ID=${CF_APP}"-"$ORG_ID
+Host_ID=$APP_ID"-"$ORG_ID
 echo "Prepared to push using" $Host_ID
 
 # Push using cf push <App-name> -n <custom_host_ID>
-cf push ${CF_APP} -n $Host_ID
+# cf push ${CF_APP} -n $Host_ID
+
+cf push $APP_ID -n $Host_ID -m 128M
 
 # View logs
 EXIT_CODE=$?
